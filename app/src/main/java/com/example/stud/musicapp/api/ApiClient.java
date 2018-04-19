af6faae6b05bbc17e24a6aff951cf702105cd989
W57012 <patrykalduk96@wp.pl>
2018-04-19 18:43:12 +0200

    Elementy

--- app/src/main/java/com/example/stud/musicapp/api/ApiClient.java
+++ app/src/main/java/com/example/stud/musicapp/api/ApiClient.java
@@ -1,11 +1,16 @@
 package com.example.stud.musicapp.api;
 
-import retrofit2.Retrofit;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 /**
  * Created by W57012 on 2018-04-19.
  */
 
 public interface ApiClient {
+    @GET( "trending.php" )
+    Call<TrendingList> getTrendingList(@Query( "country" ) String country, @Query ( "type" )
+            String type, @Query ( "format" ) String format);
 
 }
--- app/src/main/java/com/example/stud/musicapp/api/ApiService.java
+++ app/src/main/java/com/example/stud/musicapp/api/ApiService.java
@@ -10,7 +10,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiService {
     public static ApiClient getService() {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl( "http://www.theaudiodb.com/api/v1/json/{APIKEY}/" )
+                .baseUrl( "http://www.theaudiodb.com/api/v1/json/1/" )
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         return retrofit.create(ApiClient.class );
