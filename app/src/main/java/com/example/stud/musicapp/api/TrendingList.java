5a086aa2183b7b50c8a3d11b7a3ade7496616fe0
W57012 <patrykalduk96@wp.pl>
2018-04-19 18:09:38 +0200

    Modele

---
+++ app/src/main/java/com/example/stud/musicapp/api/TrendingList.java
@@ -0,0 +1,12 @@
+package com.example.stud.musicapp.api;
+
+import java.util.List;
+
+/**
+ * Created by W57012 on 2018-04-19.
+ */
+
+public class TrendingList {
+
+    public List<TrendingSingle> trending;
+}
