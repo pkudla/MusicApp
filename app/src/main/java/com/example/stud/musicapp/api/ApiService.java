7988f7ae24777bae1163922f0bb100572ff49523
W57012 <patrykalduk96@wp.pl>
2018-04-19 18:14:27 +0200

    GSon

--- app/src/main/java/com/example/stud/musicapp/api/ApiService.java
+++ app/src/main/java/com/example/stud/musicapp/api/ApiService.java
@@ -1,6 +1,7 @@
 package com.example.stud.musicapp.api;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by W57012 on 2018-04-19.
@@ -10,6 +11,7 @@ public class ApiService {
     public static ApiClient getService() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl( "http://www.theaudiodb.com/api/v1/json/{APIKEY}/" )
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
         return retrofit.create(ApiClient.class );
     }
