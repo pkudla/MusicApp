9b0263005b0440c8f75ead074f56a6c5463d19c1
W57012 <patrykladuk96@wp.pl>
2018-04-26 18:13:06 +0200

    Dokonczenie listy

--- app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
@@ -2,14 +2,20 @@ package com.example.stud.musicapp.topsongs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.stud.musicapp.R;
 import com.example.stud.musicapp.api.ApiService;
 import com.example.stud.musicapp.api.TrendingList;
+import com.example.stud.musicapp.api.TrendingSingle;
 import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,6 +24,7 @@ import retrofit2.Response;
 public class TopSongsActivity extends AppCompatActivity {
 
     RecyclerView rvList;
+    List<TrendingSingle> trendingSingles = new ArrayList<>(0);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,18 +34,25 @@ public class TopSongsActivity extends AppCompatActivity {
 
         rvList = findViewById(R.id.rvList);
 
+        TopSongsAdapter topSongsAdapter = new TopSongsAdapter(trendingSingles);
+        rvList.setAdapter(topSongsAdapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        rvList.setLayoutManager(linearLayoutManager);
+
         Call<TrendingList> trendingListCall = ApiService.getService()
                 .getTrendingList("us", "itunes", "singles");
         trendingListCall.enqueue(new Callback<TrendingList>() {
             @Override
             public void onResponse(Call<TrendingList> call, Response<TrendingList> response) {
                 TrendingList trendingList = response.body();
-
-                Toast.makeText(TopSongsActivity.this, "Elemenntow: " + trendingList.trending.size(),
-                        Toast.LENGTH_SHORT).show();
-
-                Log.d("TAG", new Gson().toJson(trendingList));
-
+                if(trendingList != null) {
+                    TopSongsActivity.this.trendingSingles.clear();
+                    TopSongsActivity.this.trendingSingles.addAll(trendingList.trending);
+                    TopSongsActivity.this.rvList.getAdapter().notifyDataSetChanged();
+                }
 
             }
             @Override
