4828cdce4b2ff2838c1572615dc6700647805368
W57012 <patrykalduk96@wp.pl>
2018-05-17 17:53:19 +0200

    Dokonczenie widoku szczegolow

--- app/src/main/java/com/example/stud/musicapp/topsongs/SongDetailsActivity.java
+++ app/src/main/java/com/example/stud/musicapp/topsongs/SongDetailsActivity.java
@@ -3,10 +3,14 @@ package com.example.stud.musicapp.topsongs;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.stud.musicapp.R;
 import com.example.stud.musicapp.api.ApiService;
+import com.example.stud.musicapp.api.Track;
 import com.example.stud.musicapp.api.Tracks;
 
 import retrofit2.Call;
@@ -38,16 +42,37 @@ public class SongDetailsActivity extends AppCompatActivity {
         ApiService.getService().getTrack(trackId).enqueue(new Callback<Tracks>() {
             @Override
             public void onResponse(Call<Tracks> call, Response<Tracks> response) {
+                Tracks tracks = response.body();
 
-                Toast.makeText(SongDetailsActivity.this, "Pobrano dane", Toast.LENGTH_SHORT).show();
+                if (tracks != null && tracks.track.size() > 0) {
+                    showData(tracks.track.get(0));
+                }
             }
-
             @Override
             public void onFailure(Call<Tracks> call, Throwable t) {
-                Toast.makeText(SongDetailsActivity.this, "Błąd pobierania danych: " + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(SongDetailsActivity.this,
+                        "Błąd pobierania danych: " + t.getLocalizedMessage(),
+                        Toast.LENGTH_SHORT).show();
             }
         });
     }
+    private void showData(Track track) {
+        TextView tvAlbum = findViewById(R.id.tvAlbum);
+        TextView tvGenre = findViewById(R.id.tvGenre);
+        TextView tvStyle = findViewById(R.id.tvStyle);
+        TextView tvDescription = findViewById(R.id.tvDescription);
+        tvAlbum.setText(track.strAlbum);
+        tvGenre.setText(track.strGenre);
+        tvStyle.setText(track.strStyle);
+        tvDescription.setText(track.strDescriptionEN);
+        if (track.strTrackThumb != null && !track.strTrackThumb.isEmpty()) {
+            ImageView ivThumb = findViewById(R.id.ivThumb);
+            Glide.with(this).load(track.strTrackThumb).into(ivThumb);
+        }
+
+    }
+
+
     public boolean onSupportNavigateUp() {
         onBackPressed();
 
