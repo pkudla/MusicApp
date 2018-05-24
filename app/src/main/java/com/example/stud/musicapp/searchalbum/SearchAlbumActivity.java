4703908d67d09555b9ebac3bb298a831cb975654
W57012 <patrykalduk96@wp.pl>
2018-05-24 18:59:09 +0200

    zapamietanie frazy

--- app/src/main/java/com/example/stud/musicapp/searchalbum/SearchAlbumActivity.java
+++ app/src/main/java/com/example/stud/musicapp/searchalbum/SearchAlbumActivity.java
@@ -1,11 +1,21 @@
 package com.example.stud.musicapp.searchalbum;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.stud.musicapp.R;
 
 public class SearchAlbumActivity extends AppCompatActivity {
+
+    EditText etQuery;
+    RecyclerView rvList;
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +24,32 @@ public class SearchAlbumActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        sharedPreferences = getPreferences(MODE_PRIVATE);
 
+
+        etQuery = findViewById(R.id.etQuery);
+        rvList = findViewById(R.id.rvList);
+
+        String artist = sharedPreferences.getString("query", null);
+        etQuery.setText(artist);
+
+        Button bSearch = findViewById(R.id.bSearch);
+        bSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String query = etQuery.getText().toString();
+                rememberQuery(query);
+            }
+        });
     }
-     @Override
+
+    public void rememberQuery(String query) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("query", query);
+        editor.apply();
+    }
+
+    @Override
     public boolean onSupportNavigateUp() {
 
         onBackPressed();
