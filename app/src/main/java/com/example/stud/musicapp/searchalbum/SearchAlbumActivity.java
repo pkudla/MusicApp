9686b71251544c2de4c69de6895083c1b6261cab
W57012 <patrykalduk96@wp.pl>
2018-06-14 18:58:30 +0200

    Wyświetlanie listy

--- app/src/main/java/com/example/stud/musicapp/searchalbum/SearchAlbumActivity.java
+++ app/src/main/java/com/example/stud/musicapp/searchalbum/SearchAlbumActivity.java
@@ -4,16 +4,27 @@ import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.stud.musicapp.R;
 import com.example.stud.musicapp.api.ApiService;
+import com.example.stud.musicapp.api.SearchAlbum;
 import com.example.stud.musicapp.api.SearchAlbums;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,6 +36,9 @@ public class SearchAlbumActivity extends AppCompatActivity {
     RecyclerView rvList;
 
     SharedPreferences sharedPreferences;
+
+
+    List<SearchAlbum> albums = new ArrayList<>(0);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +52,12 @@ public class SearchAlbumActivity extends AppCompatActivity {
 
         etQuery = findViewById(R.id.etQuery);
         rvList = findViewById(R.id.rvList);
+
+        SearchAlbumAdapter searchAlbumAdapter = new SearchAlbumAdapter(albums);
+        rvList.setAdapter(searchAlbumAdapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvList.setLayoutManager(linearLayoutManager);
 
         try {
             etQuery.setText(sharedPreferences.getInt("query", 0));
@@ -85,7 +105,7 @@ public class SearchAlbumActivity extends AppCompatActivity {
                     return;
                 }
 
-                Toast.makeText(SearchAlbumActivity.this, "Znaleziono " + searchAlbums.album.size() + " wyników", Toast.LENGTH_SHORT).show();
+                upDateList(searchAlbums);
             }
 
             @Override
@@ -95,6 +115,13 @@ public class SearchAlbumActivity extends AppCompatActivity {
         });
     }
 
+    private void upDateList(SearchAlbums searchAlbums) {
+        albums.clear();
+        albums.addAll(searchAlbums.album);
+
+        rvList.getAdapter().notifyDataSetChanged();
+    }
+
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -102,5 +129,95 @@ public class SearchAlbumActivity extends AppCompatActivity {
         onBackPressed();
         return true;
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.sort_albums_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemSortName:
+                sortByName();
+                return true;
+            case R.id.itemSortYear:
+                sortByYear();
+                return true;
+            case R.id.itemSortSales:
+                sortBySales();
+                return true;
+            case R.id.itemSortScore:
+                sortByScore();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void sortByName() {
+        Collections.sort(albums, new Comparator<SearchAlbum>() {
+            @Override
+            public int compare(SearchAlbum album1, SearchAlbum album2) {
+                if (album1.strAlbum.equals(album2.strAlbum)) {
+                    return 0;
+                }
+
+                return album1.strAlbum.compareTo(album2.strAlbum);
+            }
+        });
+
+        rvList.getAdapter().notifyDataSetChanged();
+
+    }
+
+    private void sortByYear() {
+        Collections.sort(albums, new Comparator<SearchAlbum>() {
+            @Override
+            public int compare(SearchAlbum album1, SearchAlbum album2) {
+                if (album1.intYearReleased == album2.intYearReleased) {
+                    return 0;
+                }
+
+                return album1.intYearReleased < album2.intYearReleased ? -1 : 1;
+            }
+        });
+
+        rvList.getAdapter().notifyDataSetChanged();
+    }
+
+    private void sortBySales() {
+        Collections.sort(albums, new Comparator<SearchAlbum>() {
+            @Override
+            public int compare(SearchAlbum album1, SearchAlbum album2) {
+                if (album1.intSales == album2.intSales) {
+                    return 0;
+                }
+
+                return album1.intSales < album2.intSales ? -1 : 1;
+            }
+        });
+
+        rvList.getAdapter().notifyDataSetChanged();
+    }
+
+    private void sortByScore() {
+        Collections.sort(albums, new Comparator<SearchAlbum>() {
+            @Override
+            public int compare(SearchAlbum album1, SearchAlbum album2) {
+                if (album1.intScore == album2.intScore) {
+                    return 0;
+                }
+
+                return album1.intScore < album2.intScore ? -1 : 1;
+            }
+        });
+
+        rvList.getAdapter().notifyDataSetChanged();
+    }
+
+
 
 }
